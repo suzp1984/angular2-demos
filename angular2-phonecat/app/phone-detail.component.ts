1ead6f2c2c46849a973edb2b52345628fc4644ac
suzp1984 <suzp1984@gmail.com>
2016-06-28 09:57:51 +0800

    debug in phone-detail component

--- angular2-phonecat/app/phone-detail.component.ts
+++ angular2-phonecat/app/phone-detail.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { RouteParams } from '@angular/router-deprecated';
+import { PhoneService } from './phone.service';
 
 @Component({
     selector: 'phone-detail',
     styleUrls: ['app/phone-detail.component.css'],
-    templateUrl: 'app/phone-detail.component.html'
+    templateUrl: 'app/phone-detail.component.html',
+    providers: [PhoneService]
 })
 
 export class PhoneDetailComponent {
+    title = "Angular2 Phone Detail";
+    
+    constructor(private routeParams: RouteParams,
+                private phoneService: PhoneService) {
+    }
+
+    ngOnInit() {
+        if (this.routeParams.get('id') != null) {
+            let id = this.routeParams.get('id');
+
+            // get id from phone.service
+        }
+    }
 };
